f4f8376103e214ad7d005bae5e37017789bc4f05
KraSS <groverkras@gmail.com>
2024-05-28 16:58:36 +0300

    Create PasswordHasher class

---
+++ Entities/PasswordHasher.cs
@@ -0,0 +1,11 @@
+﻿namespace modLib.Entities
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password) =>
+            BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+
+        public bool Verify(string password, string hashPassword) =>
+            BCrypt.Net.BCrypt.EnhancedVerify(password, hashPassword);
+    }
+}

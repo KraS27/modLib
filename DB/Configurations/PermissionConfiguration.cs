ea269fb6eaae6c1f8b3428afaf251cbc8b0f20b0
KraSS <groverkras@gmail.com>
2024-06-12 17:08:48 +0300

    Add PermissionConfiguration

---
+++ DB/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using modLib.Entities.DbModels;
+using modLib.Entities.Enums;
+
+namespace modLib.DB.Configurations
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<PermissionModel>
+    {
+        public void Configure(EntityTypeBuilder<PermissionModel> builder)
+        {
+            builder.ToTable("permissions");
+
+            builder.HasKey(x => x.Id);
+
+            var permissions = Enum
+                .GetValues<Permission>()
+                .Select(p => new PermissionModel
+                {
+                    Id = (int)p,
+                    Name = p.ToString(),
+                });
+
+            builder.HasData(permissions);
+        }
+    }
+}
